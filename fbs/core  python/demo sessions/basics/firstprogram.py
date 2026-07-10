92f1d722cd3299c5fbc30c5bafcf69eda1b96cd8
Harshal Ghyal <harshalghyal402@gmail.com>
2026-07-11 01:52:53 +0530

    Add files via upload

---
+++ fbs/core  python/demo sessions/basics/firstprogram.py
@@ -0,0 +1,12 @@
+print('this is the first program.') 
+
+num=10
+print(num)
+print(id(num))
+
+
+
+
+
+
+
---
+++ fbs/core  python/demo sessions/nested_if_else.py
@@ -0,0 +1,13 @@
+gender=input("enter gender(m/f):")
+age=int(input("enter age:"))
+
+if(gender=='f'):
+    if(age>=18):
+        print("girl is eligible for marriage")
+    else:
+        print("girl is not eligible for marriage") 
+else:
+    if(age>=21):
+        print("boy is eligible for marriage") 
+    else:
+        print("boy is not eligible")         
