6f04d02e5d1a78becd5a52266c43fbcba3cb6a1b
Harshal Ghyal <harshalghyal402@gmail.com>
2026-08-31 12:54:38 +0530

    Update Q03.py

--- FBS/core  python/tests/Q03.py
+++ FBS/core  python/tests/Q03.py
@@ -1,1 +1,20 @@
-#### Q
+Data = [[101, "Seema", 45000],
+        [340, "Rajani", 13000],
+        [210, "Tannu", 14000],
+        [320, "Suresh", 35000]]
+
+NewData = []
+
+salary = []
+
+for emp in Data:
+    salary.append(emp[2])
+
+salary.sort()
+
+for s in salary:
+    for emp in Data:
+        if emp[2] == s:
+            NewData.append(emp)
+
+print(NewData)
